adaf352a7668f5850440169e27c75b700d524722
Kai-Uwe Schmidt <kai-uwe.schmidt@gmx.de>
2025-10-21 21:15:01 +0200

    #9
    Bugfix sign Unittests work in progress

--- Converter/Sign.cs
+++ Converter/Sign.cs
@@ -2,18 +2,16 @@
 
 public static class Sign
 {
-    public static void SignDocument(string fileToSign)
+    public static void SignDocument(string fileToSign, string pfxFile, string password)
     {
         // The path to the documents directory
-#pragma warning disable S1075 // URIs should not be hardcoded
-        string pfxFile= @"C:\Git\PDFUABox\PDFSignContextHandler\pfx\PDFUABoxTestUser.pfx";
-#pragma warning restore S1075 // URIs should not be hardcoded
+        
                              // Open PDF document
         using var document = new Aspose.Pdf.Document(fileToSign);
         // Instantiate PdfFileSignature object
         using var signature = new Aspose.Pdf.Facades.PdfFileSignature(document);
         // Create PKCS#7 object for sign
-        var pkcs = new Aspose.Pdf.Forms.PKCS7(pfxFile, "password123");
+        var pkcs = new Aspose.Pdf.Forms.PKCS7(pfxFile, password);
         // Sign PDF file
         signature.Sign(1, true, new System.Drawing.Rectangle(300, 100, 400, 200), pkcs);
         // Save PDF document
