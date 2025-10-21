2ad3fe7e161f868d2c7a57eab2952276665298c3
Kai-Uwe Schmidt <kai-uwe.schmidt@gmx.de>
2025-10-21 13:28:05 +0200

    #9
    Zentralisierte Paketverwaltung und PDF-Verschlüsselung
    
    Die Paketverwaltung wurde durch die Einführung von `Directory.Packages.props` zentralisiert, um Redundanz zu reduzieren und die Wartung zu erleichtern.
    - Versionsnummern aus `PackageReference`-Elementen entfernt.
    - Neue Methoden in `Job.cs` hinzugefügt: `SetDocumentPrivileges` für PDF-Verschlüsselung und `ResetOutputStream`.
    - Nicht benötigte Abhängigkeiten entfernt (`Microsoft.VisualStudio.Azure.Containers.Tools.Targets`).
    - Dateien `Directory.Build.props` und `Directory.Packages.props` zur Lösung hinzugefügt.
    - Visual Studio-Version in der Lösung auf `17.14.36414.22` aktualisiert.
    - Neue Datei `wwwroot\gfx\PDFUABox.jpg` in `PDFUAWebApp.csproj` hinzugefügt.

--- Converter/Job.cs
+++ Converter/Job.cs
@@ -62,6 +62,38 @@ public class Job
             Status = JobStatus.Failed;
         }
 
+        ResetOutputStream();
+    }
+
+    private void SetDocumentPrivileges(string pdfFileName)
+    {
+        if (string.IsNullOrEmpty(pdfFileName))
+            throw new ArgumentNullException(nameof(pdfFileName), "pdfFileName is null or empty.");
+
+
+        using var fileSecurity = new Aspose.Pdf.Facades.PdfFileSecurity();
+        
+
+        pdfFileName = Path.Combine(Path.GetDirectoryName(pdfFileName)!, "Encrypt Test.pdf");
+        fileSecurity.BindPdf(pdfFileName);
+        //        using var pdfDocument = new Aspose.Pdf.Document(pdfFileName);
+
+        //        var documentPrivilege = Aspose.Pdf.Facades.DocumentPrivilege.ForbidAll;
+        //        documentPrivilege.AllowScreenReaders = true;
+
+        //pdfDocument.Encrypt("user", "owner", documentPrivilege, Aspose.Pdf.CryptoAlgorithm.AESx128, false);
+        //pdfDocument.Encrypt("User_P@ssw0rd", "OwnerP@ssw0rd", documentPrivilege, Aspose.Pdf.CryptoAlgorithm.AESx128, true);
+
+        fileSecurity.EncryptFile("User_P@ssw0rd", "OwnerP@ssw0rd", Aspose.Pdf.Facades.DocumentPrivilege.Print, Aspose.Pdf.Facades.KeySize.x256,
+    Aspose.Pdf.Facades.Algorithm.AES);
+
+        string encryptedFileName = Path.Combine(Path.GetDirectoryName(pdfFileName) ?? string.Empty,
+            Path.GetFileNameWithoutExtension(pdfFileName) + "_secured" + Path.GetExtension(pdfFileName));
+        //pdfDocument.Save(encryptedFileName);
+        fileSecurity.Save(encryptedFileName);
+    }
+
+    private void ResetOutputStream() { 
         OutputStream.Seek(0, SeekOrigin.Begin);
         OutputStream.Position = 0;
     }
