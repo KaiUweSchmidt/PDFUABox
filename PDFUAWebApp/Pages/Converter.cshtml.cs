c9108e44dd24d4b848d4881762d28d3b8e5b8967
Kai-Uwe Schmidt <kai-uwe.schmidt@gmx.de>
2025-10-25 11:55:22 +0200

    conversion is running on the golden path

--- PDFUAWebApp/Pages/Converter.cshtml.cs
+++ PDFUAWebApp/Pages/Converter.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PDFUABox.ConverterServices;
 
@@ -9,25 +10,77 @@ namespace PDFUABox.WebApp.Pages;
 internal partial class ConverterModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private Watcher _watcher;
+    private IWebHostEnvironment _environment;
+    private Converter _converter;
+    private readonly string _uploadFolder = string.Empty;
+    private readonly string _outputFolder = string.Empty;
 
     // LoggerMessage-Delegate für die LogInformation-Nachricht
-    private static readonly Action<ILogger, string, Exception?> _logConverterPageAccessed =
+    private static readonly Action<ILogger, string, Exception?> _logConverterOnGet =
         LoggerMessage.Define<string>(
             LogLevel.Information,
             new EventId(0, nameof(OnGet)),
-            "Converter page accessed: {Message}");
+            "{Message}");
 
-    public ConverterModel(ILogger<IndexModel> logger, Watcher watcher)
+    private static readonly Action<ILogger, string, Exception?> _logConverterOnPostAsync =
+    LoggerMessage.Define<string>(
+        LogLevel.Information,
+        new EventId(0, nameof(OnPostAsync)),
+        "{Message}");
+
+    public ConverterModel(ILogger<IndexModel> logger, IWebHostEnvironment environment, Converter converter)
     {
         _logger = logger;
-        _watcher = watcher;
+        _environment = environment;
+        _converter = converter;
+        _uploadFolder = Path.Combine(_environment.ContentRootPath, "wwwroot", "PDFUABoxFiles", "in");
+        if (Directory.Exists(_uploadFolder) == false)
+        {
+            Directory.CreateDirectory(_uploadFolder);
+        }
+        _outputFolder = Path.Combine(_environment.ContentRootPath, "wwwroot", "PDFUABoxFiles", "out");
+        if (Directory.Exists(_outputFolder) == false)
+        {
+            Directory.CreateDirectory(_outputFolder);
+        }
     }
 
-    public Watcher Watcher { get => _watcher; }
+    [BindProperty]
+    public required IFormFile Upload { get; set; }
+    public async Task<FileContentResult> OnPostAsync()
+    {
+        _logConverterOnPostAsync(_logger, $"File upload started: {Upload.FileName}", null);
+
+        var file = Path.Combine(_uploadFolder, Upload.FileName);
+
+
+        var outputFile = Path.Combine(_outputFolder, Path.GetFileNameWithoutExtension(Upload.FileName) + "_converted.pdf");
+        
+        using var fileStream = new FileStream(file, FileMode.Create);
+        await Upload.CopyToAsync(fileStream).ConfigureAwait(false);
+        var job = await _converter.CreateJob(file).ConfigureAwait(false);
+        if(job.OutputStream != null)
+        {
+            
+            
+            System.IO.File.Move(job.ResultFile, outputFile,true); //TODO: Review if this is necessary
+            using var outputFileStream = new FileStream(outputFile, FileMode.Create);
+            job.OutputStream.Position = 0;
+            await job.OutputStream.CopyToAsync(outputFileStream).ConfigureAwait(false);
+            _logConverterOnPostAsync(_logger, $"File conversion completed: {outputFile}", null);
+            outputFileStream.Close();
+            return File(
+                await System.IO.File.ReadAllBytesAsync(outputFile).ConfigureAwait(false)
+                , "application/pdf",
+                Path.GetFileName(outputFile));
+        }
+        return null!;
+    }
+
+    public Converter Converter { get => _converter; }
 
     public void OnGet()
     {
-        _logConverterPageAccessed(_logger, "started", null);
+        _logConverterOnGet(_logger, "Converter page accessed: started", null);
     }
 }
